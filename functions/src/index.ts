d24988a913ed126821ba6622f7553107c91af912
jsh9611 <jangsh9611@naver.com>
2022-11-22 18:12:03 +0900

    feat(lamda): 신청, 신청(무통장) 알림톡 firebase function 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,7 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/typescript
 //
 
+// 휴강 안내 알림톡
 exports.sendSolapiRequest = functions.https.onCall(async (data, context) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 
@@ -53,6 +54,110 @@ exports.sendSolapiRequest = functions.https.onCall(async (data, context) => {
     return {
       message: res,
       logger: "solapi에 요청을 보냅니다.",
+    };
+  });
+});
+
+// 신청완료 알림톡
+// eslint-disable-next-line max-len
+exports.enrollmentComplete = functions.https.onCall(async (data, context) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+
+  const { SolapiMessageService } = await require("solapi");
+
+  const {
+    to,
+    disableSms,
+    from,
+    studioName,
+    studioAddress,
+    instructorName,
+    genre,
+    time,
+    payment,
+  } = data;
+
+  // eslint-disable-next-line max-len
+  const messageService = new SolapiMessageService(
+    process.env.SOLAPI_API_KEY,
+    process.env.SOLAPI_SECRET_KEY
+  );
+
+  const solapiEnrollmentMessage = {
+    to,
+    from,
+    kakaoOptions: {
+      pfId: process.env.KAKAO_PFID,
+      templateId: process.env.ENROLLMENT_COMPLETE_TEMPLATE_ID,
+      variables: {
+        "#{스튜디오}": studioName,
+        "#{주소}": studioAddress,
+        "#{수업날짜}": time,
+        "#{강사명}": instructorName,
+        "#{타이틀}": genre,
+        "#{결제방법}": payment,
+      },
+      disableSms: disableSms,
+    },
+  };
+
+  // eslint-disable-next-line max-len
+  return messageService.sendOne(solapiEnrollmentMessage).then((res: unknown) => {
+    return {
+      message: res,
+      logger: "solapi에 신청완료 메세지 전송을 요청합니다.",
+    };
+  });
+});
+
+// 신청완료 무통장 알림톡
+// eslint-disable-next-line max-len
+exports.enrollmentCompleteAccountNumber = functions.https.onCall(async (data, context) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+
+  const { SolapiMessageService } = await require("solapi");
+
+  const {
+    to,
+    disableSms,
+    from,
+    studioName,
+    studioAddress,
+    instructorName,
+    genre,
+    time,
+    studioAccountNumber,
+  } = data;
+
+  // eslint-disable-next-line max-len
+  const messageService = new SolapiMessageService(
+    process.env.SOLAPI_API_KEY,
+    process.env.SOLAPI_SECRET_KEY
+  );
+
+  const solapiEnrollmentAccountNumberMessage = {
+    to,
+    from,
+    kakaoOptions: {
+      pfId: process.env.KAKAO_PFID,
+      templateId: process.env.ENROLLMENT_COMPLETE_CREDIT_ACCOUNT_TEMPLATE_ID,
+      variables: {
+        "#{스튜디오}": studioName,
+        "#{주소}": studioAddress,
+        "#{수업날짜}": time,
+        "#{강사명}": instructorName,
+        "#{타이틀}": genre,
+        "#{계좌번호}": studioAccountNumber,
+      },
+      disableSms: disableSms,
+    },
+  };
+
+  // eslint-disable-next-line max-len
+  return messageService.sendOne(solapiEnrollmentAccountNumberMessage).then((res: unknown) => {
+    return {
+      message: res,
+      logger: "solapi에 신청완료 무통장 메세지 전송을 요청합니다.",
     };
   });
 });
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 cors({
   origin: [
     "http://localhost:5500",
-    "https://us-central1-classmanager-b66fa.cloudfunctions.net/sendMessage",
+    "https://us-central1-classmanager-b66fa.cloudfunctions.net/enrollmentComplete",
   ],
   credentials: true,
 });
@@ -25,8 +25,9 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase and Firebase Authentication
 
+// sendSolapiRequest
 const testData = {
-  to: "01050946369",
+  to: "01045773013",
   disableSms: true,
   from: "01024405830",
   studioName: "Bunt",
@@ -36,6 +37,32 @@ const testData = {
   time: "12/10 13:30",
   suspended: "코로나",
   studioPhoneNumber: "025094234",
+};
+
+// enrollmentComplete
+const testData2 = {
+  to: "01045773013",
+  disableSms: true,
+  from: "01024405830",
+  studioName: "Bunt",
+  studioAddress: "경상북도 포항시 남구 지곡로 83 포스빌 6동 306호",
+  instructorName: "공태현",
+  genre: "팝핀",
+  time: "12/10 13:30",
+  payment: "현장 카드",
+};
+
+// enrollmentCompleteAccountNumber
+const testData3 = {
+  to: "01045773013",
+  disableSms: true,
+  from: "01024405830",
+  studioName: "Bunt",
+  studioAddress: "경상북도 포항시 남구 지곡로 83 포스빌 6동 306호",
+  instructorName: "공태현",
+  genre: "팝핀",
+  time: "12/10 13:30",
+  studioAccountNumber: "신한은행 110-394-175758 장성훈",
 };
 
 try {
